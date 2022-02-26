e8407c45adc74b3a46b92b8b13eecfdf01705571
Rogerio <rogerprogr@gmail.com>
2022-02-26 16:23:48 -0300

    Class Password

---
+++ Password.java
@@ -0,0 +1,43 @@
+package com.example.apppassword.activity.model;
+
+import java.io.Serializable;
+
+public class Password implements  Serializable {
+
+    private Long id;
+    private String descricao;
+    private String codigo;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
